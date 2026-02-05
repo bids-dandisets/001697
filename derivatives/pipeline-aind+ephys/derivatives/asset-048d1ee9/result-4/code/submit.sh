d6a4402aebb5c4142c85efae59197b317b11e8bd
DANDI User <info@dandiarchive.org>
2026-02-05 20:57:52 +0000

    [backups2datalad] 61 files added

---
+++ derivatives/pipeline-aind+ephys/derivatives/asset-048d1ee9/result-4/code/submit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=AIND-Ephys-Pipeline
+#SBATCH --output=/orcd/data/dandi/001/dandi-compute/processing/tmpknhx0adn/001697/derivatives/pipeline-aind+ephys/derivatives/asset-048d1ee9/result-4/logs/job-%j_slurm.log
+#SBATCH --mem=16GB
+#SBATCH --partition=mit_normal
+#SBATCH --time=12:00:00
+
+set -e
+
+NWB_FILE_PATH="/orcd/data/dandi/001/s3dandiarchive/blobs/048/d1e/048d1ee9-83b7-491f-8f02-1ca615b1d455"
+DATA_PATH="/orcd/data/dandi/001/s3dandiarchive/blobs/048/d1e"
+
+RESULTS_PATH="/orcd/data/dandi/001/dandi-compute/processing/tmpknhx0adn/001697/derivatives/pipeline-aind+ephys/derivatives/asset-048d1ee9/result-4/intermediate"
+WORKDIR="/orcd/data/dandi/001/dandi-compute/work"
+NXF_APPTAINER_CACHEDIR="/orcd/data/dandi/001/dandi-compute/work/apptainer_cache"
+
+source /etc/profile.d/modules.sh
+module load miniforge
+
+conda activate /orcd/data/dandi/001/environments/name-nextflow_environment
+
+DATA_PATH="$DATA_PATH" RESULTS_PATH="$RESULTS_PATH" NXF_APPTAINER_CACHEDIR="$NXF_APPTAINER_CACHEDIR" nextflow \
+    -C "/orcd/data/dandi/001/dandi-compute/processing/tmpknhx0adn/001697/derivatives/pipeline-aind+ephys/derivatives/asset-048d1ee9/result-4/code/mit_engaging.config" \
+    -log "/orcd/data/dandi/001/dandi-compute/processing/tmpknhx0adn/001697/derivatives/pipeline-aind+ephys/derivatives/asset-048d1ee9/result-4/logs/nextflow.log" \
+    run "/orcd/data/dandi/001/dandi-compute/aind-ephys-pipeline.cody/pipeline/main_multi_backend.nf" \
+    -work-dir "$WORKDIR" \
+    --job_dispatch_args "--input nwb --nwb-files $NWB_FILE_PATH" \
+    --nwb_ecephys_args "--backend hdf5"
+
+cd $RESULTS_PATH
+# TODO: move various items into places
+# TODO: copy capsule_version.env file into code for provenance
+# TODO: remove need for extra flags
+dandi upload --allow-any-path --validation skip
+tmpknhx0adn >> /orcd/data/dandi/001/dandi-compute/processing/done.txt
