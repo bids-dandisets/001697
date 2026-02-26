f0c6113aa9916eb85260ea90d0c426d5e64d708f
DANDI User <info@dandiarchive.org>
2026-02-26 20:41:13 +0000

    [backups2datalad] 7 files added, 1 file updated

--- derivatives/pipeline-aind+ephys/derivatives/asset-28a2b0b6/result-7/code/submit.sh
+++ derivatives/pipeline-aind+ephys/derivatives/asset-28a2b0b6/result-7/code/submit.sh
@@ -22,7 +22,7 @@ conda activate /orcd/data/dandi/001/environments/name-nextflow_environment
 DATA_PATH="$DATA_PATH" RESULTS_PATH="$RESULTS_PATH" NXF_APPTAINER_CACHEDIR="$NXF_APPTAINER_CACHEDIR" nextflow \
     -C "/orcd/data/dandi/001/dandi-compute/processing/tmpe1guido2/001697/derivatives/pipeline-aind+ephys/derivatives/asset-28a2b0b6/result-7/code/mit_engaging.config" \
     -log "/orcd/data/dandi/001/dandi-compute/processing/tmpe1guido2/001697/derivatives/pipeline-aind+ephys/derivatives/asset-28a2b0b6/result-7/logs/nextflow.log" \
-    run "/orcd/data/dandi/001/dandi-compute/aind-ephys-pipeline.cody/pipeline/main_multi_backend.nf" \
+    run "/orcd/data/dandi/001/dandi-compute/aind-ephys-pipeline.source/pipeline/main_multi_backend.nf" \
     -work-dir "$WORKDIR" \
     --job_dispatch_args "--input nwb --nwb-files $NWB_FILE_PATH" \
     --preprocessing_args "--motion skip" \
